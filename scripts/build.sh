f5d4f4165aedfb860220ea364609c1650012dda1
snnbyyds <2695096887@qq.com>
2022-09-24 13:06:59 +0800

    sdm845

---
+++ scripts/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo “Source Vars”
+export USE_CCACHE=1
+export CCACHE_SIZE=60G
+export ALLOW_MISSING_DEPENDENCIES=true
+
+# A Function to Send Posts to Telegram
+telegram_message() {
+	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
+	-d chat_id="${TG_CHAT_ID}" \
+	-d parse_mode="HTML" \
+	-d text="$1"
+}
+
+# Change to the Source Directry
+cd ~/android
+
+
+# Set-up ccache
+if [ -z "$CCACHE_SIZE" ]; then
+    ccache -M 60G
+else
+    ccache -M ${CCACHE_SIZE}
+fi
+
+# Prepare the Build Environment
+cd ~/android
+source build/envsetup.sh
+lunch sdm845-userdebug
+
+# Build!
+echo 'Start Build!'
+make -j$(nproc --all)
+
+# Exit
+exit 0
---
+++ scripts/sync.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir ~/bin
+PATH=~/bin:$PATH
+
+cd ~
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+# FUCK H
+rm -rf ~/android
+mkdir ~/android
+cd ~/android
+
+# A Function to Send Posts to Telegram
+telegram_message() {
+	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
+	-d chat_id="${TG_CHAT_ID}" \
+	-d parse_mode="HTML" \
+	-d text="$1"
+}
+
+# Clone the Sync Repo
+cd ~/android
+repo init -u git://github.com/OnePlusOSS/android.git -b oneplus6/9.0.0
+
+# Sync the Sources
+cd ~/android
+repo sync -j6
+
+exit 0
